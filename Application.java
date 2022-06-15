8512041c331cfac35f768bd8c41c6f1b9f00a30a
tototrois <developdeux@yahoo.com>
2022-06-15 20:06:45 +0200

    Squelette de code v1.1 + commentaires

--- Application.java
+++ Application.java
@@ -3,10 +3,16 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("3e2e234c-7664-4ae6-b4dc-382ef859cf6e")
 public class Application {
+    /**
+     * Appelle la méthode saisie() pour demander à l'utilisateur de saisir son nom et son prénom sur la console d’entrée et affiche sur la console de sortie le message "Bonjour" suivi de son nom et son prénom.
+     */
     @objid ("07a16819-5976-437d-b6ca-5f5984e7a79b")
     public static void main(List<String> args) {
     }
 
+    /**
+     * Affiche l'invite spécifiée par le paramètre message puis lit au clavier une chaîne saisie par l'utilisateur et la renvoie si elle est non vide. Boucle tant que la chaîne saisie est vide.
+     */
     @objid ("c749bfc3-52c3-4249-bb1a-d34ead6e9394")
     private static String saisie(String message) {
     }
