a31e8b121f6ea5633d9d22c5efcc6ba6e4acc86e
tototrois <developdeux@yahoo.com>
2022-06-15 19:57:28 +0200

    Squelette de code v1.1

--- Application.java
+++ Application.java
@@ -1,11 +1,14 @@
 import java.util.List;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
+@objid ("3e2e234c-7664-4ae6-b4dc-382ef859cf6e")
 public class Application {
-    /**
-     * Affiche "Bonjour" à l'écran (console de sortie).
-     */
+    @objid ("07a16819-5976-437d-b6ca-5f5984e7a79b")
     public static void main(List<String> args) {
-        System.out.println("Bonjour !");
+    }
+
+    @objid ("c749bfc3-52c3-4249-bb1a-d34ead6e9394")
+    private static String saisie(String message) {
     }
 
 }
